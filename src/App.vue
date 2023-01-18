89857b2e28811a180a21513d54bdddd4139aecee
lxy <longxinyu2021@163.com>
2023-01-18 11:33:06 +0800

    improvement: modify main.js App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,84 @@
 <template>
-  <div class="container">
-    <h1>Welcome to Tauri!</h1>
-
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-    </div>
-
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <p>
-      Recommended IDE setup:
-      <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-      +
-      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-      +
-      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
-        >Tauri</a
-      >
-      +
-      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank"
-        >rust-analyzer</a
-      >
-    </p>
-  </div>
+	<div class="container">
+		<div class="menus">
+			<Menus />
+		</div>
+		<div class="body">
+			<nav class="nav">
+				<a href="#">后退</a>
+				<a href="#">前进</a>
+				<a href="#">用户名</a>
+			</nav>
+			<RouterView class="box"/>
+		</div>
+		<div class="player">播放栏</div>
+	</div>
 </template>
 
 <script setup>
+import "./assets/css/base.css";
+import Menus from "./components/Menus.vue";
 </script>
 
-<style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
+<style scoped lang="less">
+.container {
+	display: grid;
+	grid-template-columns: repeat(9, 1fr);
+	grid-template-rows: repeat(19, 1fr);
+	background-color: #00000000;
+	width: 100%;
+	height: 100%;
+	overflow: auto;
 
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
+	.menus {
+		grid-column-start: 1;
+		grid-column-end: 2;
+		grid-row-start: 1;
+		grid-row-end: 18;
+		// background-color: #000000;
+		background-color: #22212c;
+		border-right: 1px solid #282828;
+	}
+
+	.body {
+		grid-column-start: 2;
+		grid-column-end: 10;
+		grid-row-start: 1;
+		grid-row-end: 18;
+		// background-color: #121212;
+		background-color: #151320;
+		overflow: auto;
+		display: flex;
+		flex-direction: column;
+
+		.nav {
+			display: flex;
+			flex-grow: 0;
+			// height: 100px;
+			margin-top: 10px;
+
+			a {
+				margin-left: 20px;
+			}
+		}
+
+		.body {
+			display: flex;
+			flex-grow: 1;
+		}
+	}
+
+	.player {
+		grid-column-start: 1;
+		grid-column-end: 10;
+		grid-row-start: 18;
+		grid-row-end: 20;
+		background-color: #181818;
+		border-top: 1px solid #282828;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
 }
 </style>

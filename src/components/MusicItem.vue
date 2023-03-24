aad2572c49df9d56475d20f7825bda3ee7c37c07
lxy <longxinyu2021@163.com>
2023-03-24 11:22:14 +0800

    improvement: change music item layout to grid

--- src/components/MusicItem.vue
+++ src/components/MusicItem.vue
@@ -1,7 +1,12 @@
 <template>
-    <div class="container">
+    <div class="container-music-item">
         <div class="collect">
-            <div class="id">{{ id }}</div>
+            <div class="index">
+                <span class="id">{{ id }}</span>
+                <span class="index-icon">
+                    <i class="fa fa-play fa-play" />
+                </span>
+            </div>
             <div class="cover">
                 <img :src="info.picUrl">
             </div>
@@ -39,7 +44,7 @@ const props = defineProps({
 
 onBeforeMount(() => {
     let dt_s = Math.floor(props.info.dt / 1000.0);
-    
+
     time.value.minute = Math.floor(dt_s / 60);
     time.value.second = Math.floor(dt_s - time.value.minute * 60);
 
@@ -60,30 +65,37 @@ onBeforeMount(() => {
 </script>
 
 <style scoped lang="less">
-.container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    flex-direction: row;
-    padding: 10px;
-    opacity: 0.8;
+.container-music-item {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: 1fr;
+    grid-column-gap: 0px;
+    grid-row-gap: 0px;
+    padding: 5px;
 
     .collect {
+        grid-area: 1 / 1 / 2 / 3;
         display: flex;
         align-items: center;
-        justify-content: center;
+        justify-content: flex-start;
         flex-direction: row;
+        margin-left: 10px;
 
-        .id {
+        .index {
             display: flex;
             align-items: center;
             margin: 2px;
+
+            .index-icon {
+                display: none;
+            }
         }
 
         .cover {
             display: flex;
             align-items: center;
             margin: 2px;
+            margin-left: 20px;
 
             img {
                 width: 50px;
@@ -93,39 +105,46 @@ onBeforeMount(() => {
 
         .info {
             display: flex;
-            align-items: center;
+            align-items: flex-start;
             margin: 2px;
             flex-direction: column;
+            margin-left: 20px;
 
             .name {
                 display: flex;
-                align-items: center;
+
             }
 
             .artists {
                 display: flex;
-                align-items: center;
             }
         }
     }
 
     .album {
+        grid-area: 1 / 3 / 2 / 4;
         display: flex;
         align-items: center;
         margin: 2px;
     }
 
     .time {
+        grid-area: 1 / 4 / 2 / 5;
         display: flex;
         align-items: center;
+        justify-content: flex-end;
         margin: 2px;
     }
 }
 
-.container:hover {
+.container-music-item:hover {
     border-radius: 10px;
     background-color: #2a2a2a;
     cursor: pointer;
     opacity: 1;
+}
+
+.container-music-item:hover .collec .index .id {
+        display: none !important;
 }
 </style>
